d7813495de68d9f8b56b4f617c8a6cd19738aa3d
o809054054 <34680678+o809054054@users.noreply.github.com>
2018-01-10 21:19:37 +0900

    Add files via upload

---
+++ table.php
@@ -0,0 +1,38 @@
+ <?php
+    
+    //テーブルGuestInfoはアカウントを保存しているもの
+    /*
+    $sql = "CREATE TABLE GuestInfo(
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        userid VARCHAR(30) NOT NULL,
+        password VARCHAR(30) NOT NULL,
+        date TIMESTAMP
+    )";
+    $pdo->exec($sql);
+    echo "TABLE GuestInfo created successfully";
+
+    $sql = "ALTER TABLE GuestInfo 
+    ADD email VARCHAR(255),
+    ADD uniqueid VARCHAR(255)";
+    $result = $pdo->query($sql);
+    */ 
+
+    //テーブルMyGuestsは投稿した内容を保存しているもの
+    /*
+    $sql = "CREATE TABLE MyGuests(
+
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(30) NOT NULL,
+        comment VARCHAR(255) NOT NULL,
+        password VARCHAR(30) NOT NULL,
+        date TIMESTAMP
+
+    )";
+    $pdo->exec($sql);
+    
+    $sql = "ALTER TABLE MyGuests 
+            ADD image VARCHAR(255),
+            ADD video VARCHAR(255)";
+    $result = $pdo->query($sql);
+    */
+?>
